55d6f91f5521afa466bc864f96663c6842a0e7ad
Michael Sartain <mikesart@fastmail.com>
2022-04-08 20:53:55 -0600

    Add pc, sp, fp values
    Also add frame number + count

--- stack-inspector.py
+++ stack-inspector.py
@@ -1,6 +1,8 @@
 import gdb
 from collections import OrderedDict, namedtuple
 
+## frame unwinder is better for this?
+##   https://sourceware.org/gdb/onlinedocs/gdb/Unwinding-Frames-in-Python.html#Unwinding-Frames-in-Python
 
 ANSI_BOLD = "\x1b[1m"
 ANSI_GREEN = "\x1b[32m"
@@ -19,7 +21,7 @@ def analyze_frame(frame_nr, frame):
         print("  {bold}#{frame_nr:<3}{reset} "
               "{green}{function}{reset}"
               " @ "
-              "{filename}:{line}\n".format(
+              "{filename}:{line}".format(
                 frame_nr=frame_nr,
                 filename=info.symtab.filename,
                 line=info.line,
@@ -28,11 +30,23 @@ def analyze_frame(frame_nr, frame):
                 green=ANSI_GREEN,
                 reset=ANSI_RESET))
     else:
-        print("  {bold}#{frame_nr:<3}{reset} Could not retrieve frame information\n".format(
+        print("  {bold}#{frame_nr:<3}{reset} Could not retrieve frame information".format(
             frame_nr=frame_nr,
             bold=ANSI_BOLD,
             green=ANSI_GREEN,
             reset=ANSI_RESET))
+
+    # $pc: program counter register
+    # $sp: stack pointer
+    # $fp: pointer to current stack frame
+    print("{space}pc: 0x{pc:x}\n{space}sp: {sp}\n{space}fp: {fp}".format(
+            pc=frame.pc(),
+            sp=frame.read_register("sp"),
+            fp=frame.read_register("fp"),
+            space="           "
+            ))
+
+    if not info.symtab:
         return
 
     try:
@@ -88,13 +102,24 @@ class StackVisualizer(gdb.Command):
 
         backtrace = []
 
+        fstart = 0
+        fend = 999999
+        argv = gdb.string_to_argv(arg)
+        if ( len(argv) > 1 ):
+            fstart = int(gdb.parse_and_eval(argv[0]))
+            fend = fstart + int(gdb.parse_and_eval(argv[1])) - 1
+        elif ( len(argv) > 0 ):
+            fend = int(gdb.parse_and_eval(argv[0])) - 1
+
         while frame:
             backtrace.append(frame)
             frame = frame.older()
 
         print()
         for frame_nr, frame in enumerate(backtrace):
-            analyze_frame(frame_nr, frame)
-
+            if frame_nr >= fstart:
+                analyze_frame(frame_nr, frame)
+            if frame_nr >= fend:
+                break
 
 StackVisualizer()
